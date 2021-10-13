33600dcb50b63883c8e0a7ad360a8949f4e6137c
Timea Turdean <timea.turdean@gmail.com>
2021-10-13 17:29:45 +0200

    dep updates which run with node 12 and 15

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ["./jest.setup.ts"],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
+  }
 };
