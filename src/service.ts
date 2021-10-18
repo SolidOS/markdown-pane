837b431f750d28ab30c7e57c5f4a3afdf697f237
Angelo Veltens <angelo.veltens@codecentric.de>
2021-10-18 23:46:16 +0200

    explicitly request text/markdown

--- src/service.ts
+++ src/service.ts
@@ -1,13 +1,23 @@
-import { IndexedFormula } from 'rdflib'
+import { IndexedFormula } from "rdflib";
 
-export function loadMarkdown (store: IndexedFormula, uri: string): Promise<string> {
-  return store.fetcher.webOperation('GET', uri)
-    .then((response) => response.responseText)
+export function loadMarkdown(
+  store: IndexedFormula,
+  uri: string
+): Promise<string> {
+  return store.fetcher
+    .webOperation("GET", uri, {
+      headers: { accept: "text/markdown" },
+    })
+    .then((response) => response.responseText);
 }
 
-export function saveMarkdown (store: IndexedFormula, uri: string, data: string): Promise<Response>  {
-  return store.fetcher.webOperation('PUT', uri, {
+export function saveMarkdown(
+  store: IndexedFormula,
+  uri: string,
+  data: string
+): Promise<Response> {
+  return store.fetcher.webOperation("PUT", uri, {
     data,
-    contentType: 'text/markdown; charset=UTF-8'
-  })
+    contentType: "text/markdown; charset=UTF-8",
+  });
 }
